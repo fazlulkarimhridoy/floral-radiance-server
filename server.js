f7d7ea3f0aedff6433290195f2f20d8aba363b88
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-10-15 01:59:27 +0600

    added server checking log for root route

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ app.use("/api", routes);
 
 const PORT = process.env.PORT || 3000;
 
+//check if server running and connect to database
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+prisma
+    .$connect()
+    .then(() => {
+        console.log("Connected to the database");
+    })
+    .catch((error) => {
+        console.error("Error connecting to the database", error);
+    });
+
+// server status
+app.get("/", (req, res) => {
+    res.send("Floral Radiance server is running");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
